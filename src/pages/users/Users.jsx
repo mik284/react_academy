fa06a29565a6677c3a0f14b895fe4a9c62bb8727
mik284 <mikeykamau222@gmail.com>
2024-01-09 14:47:01 +0300

    displayed users

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,14 +1,60 @@
-import { useEffect } from "react"
-import fetchUsers from "../../services/userApi"
-
+import { useEffect, useState } from "react";
+import fetchUsers from "../../services/userApi";
 
 function Users() {
-    useEffect(() => {
-    fetchUsers()
-    }, [])
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const fetchedUsers = await fetchUsers();
+      setData(fetchedUsers);
+    };
+    getUsers();
+  }, []);
+
   return (
-    <div>Users</div>
-  )
+    <>
+      <div>
+        <h3>Current users.</h3>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+          }}
+        >
+          {data?.map((user) => {
+            return (
+              <div
+                key={user.id}
+                style={{
+                  border: "1px solid grey",
+                  borderRadius: 5,
+                  gap: 2,
+                  width: 200,
+                  height: 250,
+                  margin: "0 auto",
+                  marginBottom: 10,
+                  padding: 10,
+                }}
+              >
+                <div>
+                    <div>
+                        👤
+                    </div>
+                  <h4>Name: {user.name}</h4>
+                  <span>Username: {user.username}</span>
+                  <p>☎️ {user.phone}</p>
+                  <i>🌐 <a href={`http://${user.website}`}>{user.website}</a></i>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Users
+export default Users;
--- src/services/userApi.js
+++ src/services/userApi.js
@@ -3,7 +3,6 @@ import { BASE_URL } from "../utils/config";
 const fetchUsers = async () => {
   const response = await fetch(`${BASE_URL}/users`);
   const data = await response.json();
-  console.log(data);
   return data;
 };
 
