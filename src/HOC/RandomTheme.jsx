c87e5be29f6053f208f796ff0be1189c955ee0ac
mik284 <mikeykamau222@gmail.com>
2024-01-12 21:05:00 +0300

    modify hoc

--- src/HOC/RandomTheme.jsx
+++ src/HOC/RandomTheme.jsx
@@ -1,12 +1,14 @@
 
 
-const randomTheme = (WrappedComponent,color) => {
+const randomTheme = (WrappedComponent) => {
 
   function EnhancedComponent(props) {
-
+    const colort = ['red', 'green', 'yellow', 'orange', 'purple','skyblue']
+    const random = colort[Math.floor(Math.random()*5)]
+    
     const data = localStorage.getItem('username')
 
-    return (data ==='tracom'? <div style={{color: `${color}`}}>
+    return (data ==='tracom'? <div style={{color: `${random}`}}>
       <WrappedComponent {...props} />
     </div>:'')
   }
